8062dbdd34e0cd78e9e0a8fbe0046731092c41da
Jeff Baldock <j.baldock1991@gmail.com>
2024-12-28 12:04:26 -0800

    add groundwater and flowline distance. remove watershed delin (does not render)

--- build_book.R
+++ build_book.R
@@ -24,3 +24,15 @@ quarto::quarto_render("BaselineFst/BaselineFst.qmd",
 quarto::quarto_render("GSI Analysis/GSIAnalysis.qmd", 
                       #cache_refresh = TRUE, # default is FALSE
                       output_format = "html")
+
+quarto::quarto_render("Landscape Covariates/Watershed Delineation/WatershedDelineation.qmd", 
+                      cache_refresh = TRUE, # default is FALSE
+                      output_format = "html")
+
+quarto::quarto_render("Landscape Covariates/Flowline Distance/FlowlineDist.qmd", 
+                      cache_refresh = TRUE, # default is FALSE
+                      output_format = "html")
+
+quarto::quarto_render("Landscape Covariates/Groundwater/GroundwaterIndex.qmd", 
+                      cache_refresh = TRUE, # default is FALSE
+                      output_format = "html")
